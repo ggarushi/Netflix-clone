d574eff2f0b7f55b35b74519dc36fae9b9efd6bf
ggarushi <garushiarora1999@gmail.com>
2021-02-23 12:05:25 +0530

    added row

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Banner from './Banner';
+import request from './Request';
+import requests from './Request';
 function Home(){
     return <div>
         {/*Navbar*/}
         <Navbar/>
-        <Banner/>  
+        <Banner/> 
+        <Row title="NETFLIX ORIGINALS" fetchedUrl={requests.fetchNetflixOriginals} isLarge/> 
+        <Row title="TOP TRENDING" fetchedUrl={requests.fetchTrending} /> 
+        <Row title="TOP RATED" fetchedUrl={requests.fetchTopRated} /> 
+        <Row title="ACTION MOVIES" fetchedUrl={requests.fetchActionMovies} />
+        <Row title="HORROR MOVIES" fetchedUrl={requests.fetchHorrorMovies} />  
+        <Row title="COMEDY MOVIES" fetchedUrl={requests.fetchComedyMovies} /> 
+        <Row title="ROMANTIC MOVIES" fetchedUrl={requests.fetchRomanceMovies} /> 
+        <Row title="DOCUMENTARIES" fetchedUrl={requests.fetchDocumentaries} /> 
             
     </div>
 }
