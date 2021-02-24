dd85de2385e1ed13448f8299493d6f761efe2e04
ggarushi <garushiarora1999@gmail.com>
2021-02-24 18:53:51 +0530

    added signup

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Login.css';
+import Signup from './Signup';
 function Login(){
+    const[signIn,setSignin]=useState(false);
     return <div className="login">
                 <div className="login_content">
                     <img 
                     className="login_logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" 
                     alt="Netflix Logo"/>
-                    <button className="login_button">Sign In</button>
                 <div className="login_gradient"></div>
+                {/* <button className="login_button"  onClick={()=>setSignin(true)}>Sign In</button> */}
                 </div>
                 <div className="login_card">
+                    {signIn?(<Signup/>):(
+                        <>
+                            <h1 className="login_title">Unlimited movies, TV shows and more.</h1>
+                            <h2 className="subtitle">Watch anywhere. Cancel anytime.</h2>
+                            <h3>Ready to watch? Enter your 
+                                email to create or restart your membership.</h3>
+                            <button className="login_started">Get Started</button>
+                            <button className="login_button"  onClick={()=>setSignin(true)}>Sign In</button>
+                        </>
+                    )}
                     
-                    <h1 className="login_title">Unlimited movies, TV shows and more.</h1>
-                    <h2 className="subtitle">Watch anywhere. Cancel anytime.</h2>
-                    <h3>Ready to watch? Enter your 
-                        email to create or restart your membership.</h3>
-                    <button className="login_started">Get Started</button>
                     
                 </div>
             </div>
 }
 export default Login;
 // "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-kids-halloween-movies-for-netflix-1604009108.jpg"
+
