8ed5f005ce36757cee413fe80b7e7f6aec437cf7
ggarushi <garushiarora1999@gmail.com>
2021-02-26 00:02:29 +0530

    Removed default styling from Link

--- src/Screens/Signup.js
+++ src/Screens/Signup.js
@@ -8,7 +8,7 @@ function Signup(){
                     <h1 style={{textAlign:"left",marginBottom:"20px"}}>Sign In</h1>
                     <input className="signup_input" placeholder="Email" type="Email"/>
                     <input className="signup_input" placeholder="Password" type="password"/>
-                    <button className="signup_button" type="submit" ><Link to="Home">Sign In</Link></button>
+                    <button className="signup_button" type="submit" ><Link to="Home" style={{textDecoration:'none',color:'white'}}>Sign In</Link></button>
                     <h4 className="signup_bottom">New to Netflix? Sign Up now.</h4>
                 </form>
 
