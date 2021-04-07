70827704215844675b6635889df9901c5a7fa053
ggarushi <garushiarora1999@gmail.com>
2021-04-08 00:58:36 +0530

    changed document-title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import './App.css';
 import Home from  './Screens/Home';
 import Login from './Screens/Login';
 
 function App() {
+  useEffect(()=>{
+    document.title="Netflix-clone"
+  },[]);
   return (
     <div className="App">
       <Router>
